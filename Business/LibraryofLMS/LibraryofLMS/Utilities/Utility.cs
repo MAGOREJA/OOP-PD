98d694385a23e3c121c681998416e1067015b392
Ayyan MAG <144407757+MAGOREJA@users.noreply.github.com>
2024-04-14 02:32:18 +0500

    ngl

---
+++ Business/LibraryofLMS/LibraryofLMS/Utilities/Utility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryofLMS.Utilities
+{
+    class Utility
+    {
+        private static string ConnectionString = "Data Source=DESKTOP-UL67J8E\\\\SQLEXPRESS;Initial Catalog=Lab;Integrated Security=True";
+
+        public static string GetConnectionString()
+        {
+            return ConnectionString;
+        }
+
+    }
+}
